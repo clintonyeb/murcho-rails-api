5fcf0addccf38886d2c764e9c59569d26f3f949c
Clinton Yeboah <clintonyeb@gmail.com>
2019-01-01 19:15:02 +0530

    deployment fixes

--- db/migrate/20181205063509_add_default_to_boolean_events.rb
+++ db/migrate/20181205063509_add_default_to_boolean_events.rb
@@ -2,10 +2,8 @@ class AddDefaultToBooleanEvents < ActiveRecord::Migration[5.2]
   def change
     remove_column :event_schemas, :is_all_day, :boolean
     remove_column :event_schemas, :is_recurring, :boolean
-    # remove_column :event_instances, :is_all_day, :boolean
 
     add_column :event_schemas, :is_all_day, :boolean, default: false
     add_column :event_schemas, :is_recurring, :boolean, default: false
-    # add_column :event_instances, :is_all_day, :boolean, default: false
   end
 end
--- db/migrate/20181205080135_add_colors_to_events.rb
+++ db/migrate/20181205080135_add_colors_to_events.rb
@@ -1,7 +1,6 @@
 class AddColorsToEvents < ActiveRecord::Migration[5.2]
   def change
     add_column :event_schemas, :color, :integer, default: 0
-    add_column :event_instances, :color, :integer, default: 0
     add_column :event_exceptions, :color, :integer, default: 0
   end
 end
